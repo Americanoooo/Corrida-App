c9b515d3800235fc79fb4d875950be05f163f86e
Americano <caua.americano0710@gmail.com>
2026-08-03 19:01:23 -0300

    api aponta pra VITE_API_URL

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 import { ApiError } from "./Erros/ApiError";
 
-const API_URL = "http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export async function apiFetch(endpoint: string, options: RequestInit = {}){
     const token = localStorage.getItem('token')
